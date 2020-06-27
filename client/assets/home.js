93b044c295958ba3d728b12a482999ddf0061b69
Tracy Vy <tracyvy88@gmail.com>
2020-06-27 10:22:36 -0700

    deploy progress

--- client/assets/home.js
+++ client/assets/home.js
@@ -7,37 +7,39 @@ $(document).ready(function () {
     renderTodos(allTodos);
   });
 });
-
-const renderTodos = (arr) = {
-  $("#card-container").html(""),
+const renderTodos = (arr) => {
+  $("#card-container").html("");
   arr.forEach((todo) => {
-    console.log(todo);
+    let msg = todo.completed ? "✔ Finished todo" : "❌ Need to do!";
     $("#card-container").prepend(
       `
       <div class="card mb-2">
-          <div class="card-body">
-            <h6 class="card-subtitle mb-2 text-muted">
-              ✅ This need to be done
-            </h6>
-            <p class="card-text">
-              ${todo.text}
-            </p>
-            <div class="text-center">
-              <button data-id=${todo.id} style="width: 150px;" class="btn btn-outline-success">
-                Edit
-              </button>
-              <button data-id=${todo.id} style="width: 150px;" class="btn btn-outline-danger">
-                Delete
-              </button>
-            </div>
+        <div class="card-body">
+          <h6 class="card-subtitle mb-2 text-muted">
+            ${msg}
+          </h6>
+          <p class="card-text mt-2">
+            ${todo.text}
+          </p>
+          <div class="text-center">
+            <button data-id=${todo.id} style="width: 150px;" class="btn btn-outline-success btnUpdate">
+              Edit
+            </button>
+            <button data-id=${todo.id} style="width: 150px;" class="btn btn-outline-danger btnDelete">
+              Delete
+            </button>
           </div>
+        </div>
       </div>
       `
     );
-  }),
+  });
 };
-
-$(document).on("click", "#btnUpdate", function () {
-  console.log($(this).attr("data-id"));
+$(document).on("click", ".btnUpdate", function () {
+  const todoId = $(this).attr("data-id");
+  window.location.href = `/edit?id=${todoId}`;
 });
-
+$(document).on("click", ".btnDelete", function () {
+  const todoId = $(this).attr("data-id");
+  window.location.href = `/delete?id=${todoId}`;
+});
